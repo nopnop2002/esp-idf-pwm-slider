3d50a7eda4d1e44a962c6a483414cbd98da8303e
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-10-03 21:24:01 +0900

    Changed TAB stop

--- main/http_server.c
+++ main/http_server.c
@@ -256,36 +256,36 @@ esp_err_t start_server(const char *base_path, int port)
 }
 
 // LEDC Stuff
-#define LEDC_TIMER				LEDC_TIMER_0
-#define LEDC_MODE				LEDC_LOW_SPEED_MODE
-//#define LEDC_OUTPUT_IO			(5) // Define the output GPIO
-#define LEDC_OUTPUT_IO			CONFIG_BLINK_GPIO // Define the output GPIO
-#define LEDC_CHANNEL			LEDC_CHANNEL_0
-#define LEDC_DUTY_RES			LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
-#define LEDC_DUTY				(4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
-#define LEDC_FREQUENCY			(5000) // Frequency in Hertz. Set frequency at 5 kHz
+#define LEDC_TIMER			LEDC_TIMER_0
+#define LEDC_MODE			LEDC_LOW_SPEED_MODE
+//#define LEDC_OUTPUT_IO	(5) // Define the output GPIO
+#define LEDC_OUTPUT_IO		CONFIG_BLINK_GPIO // Define the output GPIO
+#define LEDC_CHANNEL		LEDC_CHANNEL_0
+#define LEDC_DUTY_RES		LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY			(4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
+#define LEDC_FREQUENCY		(5000) // Frequency in Hertz. Set frequency at 5 kHz
 
 static void ledc_init(void)
 {
 	// Prepare and then apply the LEDC PWM timer configuration
 	ledc_timer_config_t ledc_timer = {
-		.speed_mode		  = LEDC_MODE,
-		.timer_num		  = LEDC_TIMER,
-		.duty_resolution  = LEDC_DUTY_RES,
-		.freq_hz		  = LEDC_FREQUENCY,  // Set output frequency at 5 kHz
-		.clk_cfg		  = LEDC_AUTO_CLK
+		.speed_mode			= LEDC_MODE,
+		.timer_num			= LEDC_TIMER,
+		.duty_resolution	= LEDC_DUTY_RES,
+		.freq_hz			= LEDC_FREQUENCY,  // Set output frequency at 5 kHz
+		.clk_cfg			= LEDC_AUTO_CLK
 	};
 	ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
 
 	// Prepare and then apply the LEDC PWM channel configuration
 	ledc_channel_config_t ledc_channel = {
-		.speed_mode		= LEDC_MODE,
-		.channel		= LEDC_CHANNEL,
-		.timer_sel		= LEDC_TIMER,
-		.intr_type		= LEDC_INTR_DISABLE,
-		.gpio_num		= LEDC_OUTPUT_IO,
-		.duty			= 0, // Set duty to 0%
-		.hpoint			= 0
+		.speed_mode			= LEDC_MODE,
+		.channel			= LEDC_CHANNEL,
+		.timer_sel			= LEDC_TIMER,
+		.intr_type			= LEDC_INTR_DISABLE,
+		.gpio_num			= LEDC_OUTPUT_IO,
+		.duty				= 0, // Set duty to 0%
+		.hpoint				= 0
 	};
 	ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 }
